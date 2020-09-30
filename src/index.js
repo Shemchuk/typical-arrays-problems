ca7506a11f861a0a940936e3195e3e33eca17e74
Mikhail Shemchuk <mihey.by@gmail.com>
2020-09-30 21:36:27 +0300

    feat: implement functions min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = function min(array) {
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
 
-exports.max = function max (array) {
-  return 0;
-}
+    let result = array.reduce((minimum, current) => {
+        if (minimum > current) {
+            minimum = current;
+        }
+        return minimum;
+    }, array[0]);
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    return result;
+};
+
+exports.max = function max(array) {
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
+
+    let result = array.reduce((maximum, current) => {
+        if (maximum < current) {
+            maximum = current;
+        }
+        return maximum;
+    }, array[0]);
+
+    return result;
+};
+
+exports.avg = function avg(array) {
+    if (arguments.length === 0 || array.length === 0) {
+        return 0;
+    }
+
+    return array.reduce((prev, current) => prev + current, 0) / array.length;
+};
+
